e8afd42aeae6f5e655656145b8a78c860688f111
Devabhaktineni Sayendranadh Chowdary <devabhaktinenisayendra@gmail.com>
2023-11-30 01:07:03 -0600

    Update Guitar.js

--- models/Guitar.js
+++ models/Guitar.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose")
 const GuitarSchema = mongoose.Schema({
 type: String,
 cost: Number,
+min:0,
+max:9999999,
 material: String
-})
+});
 module.exports = mongoose.model("Guitar",GuitarSchema)
