a7f929144aa6d45e6e1739d0ffa98a0a715657aa
Devabhaktineni Sayendranadh Chowdary <devabhaktinenisayendra@gmail.com>
2023-11-30 01:07:07 -0600

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,75 +1,54 @@
-var express = require("express");
- 
-var http = require("http-errors");
- 
-var passport = require("passport");
- 
+var express = require('express');
+var passport = require('passport');
 var router = express.Router();
+var Account = require('../models/account');
  
-var Account = require("../models/account");
- 
-router.get("/", function (req, res) {
-  res.render("index", { title: "guitar App", user: req.user });
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Guitar App', user: req.user });
 });
  
-router.get("/register", function (req, res) {
-  res.render("register", { title: "guitar App Registration" });
+router.get('/register', function (req, res) {
+  res.render('register', { title: 'Guitar App Registration' });
 });
  
-router.post("/register", async function (req, res) {
-  try {
-    const user = await Account.findOne({ username: req.body.username });
- 
-    if (user) {
-      return res.render("register", {
-        title: "Registration",
- 
-        message: "Existing User",
- 
-        account: req.body.username,
-      });
+router.post('/register', async function(req, res) {
+  try {  
+    const user = await Account.findOne({ username : req.body.username }).exec();
+    console.log('Success, redirect');
+    if(user){
+      return res.render('register', { title: 'Registration',
+            message: 'Existing User', account : req.body.username });
     }
- 
-    const newAccount = new Account({ username: req.body.username });
- 
-    await Account.register(newAccount, req.body.password);
- 
-    console.log("Success, redirect");
- 
-    res.redirect("/");
+    let newAccount = new Account({ username : req.body.username });
+    const registeredUser = await Account.register(newAccount, req.body.password);
+    console.log('Success, redirect');
+    res.redirect('/');
   } catch (err) {
     console.error(err);
- 
-    return res.render("register", {
-      title: "Registration",
- 
-      message: "Registration error",
- 
-      account: req.body.username,
-    });
+    return res.render('register', { title: 'Registration',
+            message: 'Registration error', account : req.body.username });
   }
 });
- 
-router.get("/login", function (req, res) {
-  res.render("login", { title: "guitar App Login", user: req.user });
+     
+router.get('/login', function (req, res) {
+  res.render('login', { title: 'Guitar App Login', user: req.user });
+});
+router.post('/login', passport.authenticate('local'), function (req, res) {
+  if(req.session.returnTo)
+    res.redirect(req.session.returnTo);
+  res.redirect('/');
 });
  
-router.post("/login", passport.authenticate("local"), function (req, res) {
-  if (req.session.returnTo) res.redirect(req.session.returnTo);
  
-  res.redirect("/");
-});
- 
-router.get("/logout", function (req, res, next) {
+router.get('/logout', function (req, res, next) { // use post or delete for better safety
   req.logout(function (err) {
-    if (err) {
-      return next(err);
-    }
-    res.redirect("/");
+    if (err) { return next(err); }
+    res.redirect('/');
   });
 });
  
-router.get("/ping", function (req, res) {
+router.get('/ping', function (req, res) {
   res.status(200).send("pong!");
 });
  
