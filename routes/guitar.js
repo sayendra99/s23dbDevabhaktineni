9027fbeab71628c19b2df1dc0ccef763696bf86a
Devabhaktineni Sayendranadh Chowdary <devabhaktinenisayendra@gmail.com>
2023-11-29 18:40:01 -0600

    Update guitar.js

--- routes/guitar.js
+++ routes/guitar.js
@@ -7,15 +7,19 @@ router.get('/', Guitar_controlers.Guitar_view_all_Page);
 /* GET detail Guitar page */
 router.get('/detail', Guitar_controlers.Guitar_view_one_Page);
 
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+    }
 /* GET create Guitar page */
-router.get('/create', Guitar_controlers.Guitar_create_Page);
+router.get('/create',secured,  Guitar_controlers.Guitar_create_Page);
 
 /* GET create update page */
-router.get('/update', Guitar_controlers.Guitar_update_Page);
+router.get('/update',secured,  Guitar_controlers.Guitar_update_Page);
 /* GET delete Guitar page */
-router.get('/delete', Guitar_controlers.Guitar_delete_Page);
-
-
-
+router.get('/delete',secured, Guitar_controlers.Guitar_delete_Page);
 
 module.exports = router;
